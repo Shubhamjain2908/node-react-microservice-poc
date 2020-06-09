b62f19dd98752bbd75cb9eafb6be2a2ac5ddf11e
Shubham Jain <Shubhamjain2908@gmail.com>
2020-06-10 01:34:06 +0530

    make axios req to posts

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 
 const PostCreate = () => {
+    const [title, setTitle] = useState('');
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+
+        await axios.post('http://localhost:4000/posts', {
+            title
+        });
+
+        setTitle('');
+    }
     return (
         <div>
-            <form>
+            <form onSubmit={onSubmit}>
                 <div className="form-group">
                     <label>Title</label>
-                    <input className="form-control" />
+                    <input value={title} onChange={e => setTitle(e.target.value)} className="form-control" />
                 </div>
                 <button className="btn btn-primary">Submit</button>
             </form>
