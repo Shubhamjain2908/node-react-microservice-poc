5528ef684dc4a9384f2a51cdbb9235a22d8fc26e
Shubham Jain <Shubhamjain2908@gmail.com>
2020-06-11 03:09:06 +0530

    updating comments status

--- comments/index.js
+++ comments/index.js
@@ -38,8 +38,29 @@ app.post('/posts/:id/comments', async (req, res) => {
   res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
   console.log('Event recieved: ', req.body.type);
+
+  const { type, data } = req.body;
+
+  if (type === 'CommentModerated') {
+    const { postId, id, content, status } = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find(comment => comment.id === id);
+    comment.status = status;
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        status,
+        postId,
+        content
+      }
+    });
+  }
+
   res.send({});
 });
 
