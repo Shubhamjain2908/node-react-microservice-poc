8273f1a75bdd4bb90432fb1a3455c897e353ff03
Shubham Jain <Shubhamjain2908@gmail.com>
2020-06-11 03:14:13 +0530

    rendering comments in react

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -3,8 +3,18 @@ import React from 'react';
 const CommentList = ({ comments }) => {
 
     const renderedComments = comments.map(comment => {
+        let content;
+
+        if (comment.status === 'approved') {
+            content = comment.content;
+        } else if (comment.status === 'pending') {
+            content = 'This comment is awaiting moderation';
+        } else if (comment.status === 'rejected') {
+            content = 'This comment is rejected';
+        }
+
         return (
-            <li key={comment.id}>{comment.content}</li>
+            <li key={comment.id}>{content}</li>
         );
     });
 
