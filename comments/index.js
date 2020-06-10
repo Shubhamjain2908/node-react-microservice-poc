501861606a4b099796c854212d3d1eab4922370d
Shubham Jain <Shubhamjain2908@gmail.com>
2020-06-11 01:13:42 +0530

    recieving events: post & comment

--- comments/index.js
+++ comments/index.js
@@ -37,6 +37,11 @@ app.post('/posts/:id/comments', async (req, res) => {
   res.status(201).send(comments);
 });
 
+app.post('/events', (req, res) => {
+  console.log('Event recieved: ', req.body.type);
+  res.send({});
+});
+
 app.listen(4001, () => {
   console.log('Listening on 4001');
 });
