e04dda4a1e521b12e05e2c1c3d4be342cdea26f6
Shubham Jain <shubhamjain2908@gmail.com>
2020-06-22 02:29:38 +0530

    updating client urls & added client depl in k8s

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -7,7 +7,7 @@ const PostList = () => {
     const [posts, setPosts] = useState({});
 
     const fetchPosts = async () => {
-        const res = await axios.get('http://localhost:4002/posts');
+        const res = await axios.get('http://posts.com/posts');
 
         setPosts(res.data);
     };
@@ -21,7 +21,7 @@ const PostList = () => {
             <div className="card" style={{ width: '30%', marginBottom: '20px' }} key={post.id}>
                 <div className="card-body">
                     <h3>{post.title}</h3>
-                    <CommentList comments={post.comments} />
+                    <CommentList comments={post.comments || []} />
                     <CommentCreate postId={post.id} />
                 </div>
             </div>
