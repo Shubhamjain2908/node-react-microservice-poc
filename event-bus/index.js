855e72d0042869e7a4889103b36d42cbead9cf7a
Shubham Jain <shubhamjain2908@gmail.com>
2020-06-22 00:40:51 +0530

    added k8s deployment & services to comments, query & moderation services

--- event-bus/index.js
+++ event-bus/index.js
@@ -8,14 +8,15 @@ app.use(bodyParser.json());
 const events = [];
 
 app.post('/events', (req, res) => {
-  const event = req.body;
 
+  const event = req.body;
+  console.log('Event => ', event);
   events.push(event);
 
   axios.post('http://posts-clusterip-srv:4000/events', event);
-  // axios.post('http://localhost:4001/events', event);
-  // axios.post('http://localhost:4002/events', event);
-  // axios.post('http://localhost:4003/events', event);
+  axios.post('http://comments-clusterip-srv:4001/events', event);
+  axios.post('http://query-clusterip-srv:4002/events', event);
+  axios.post('http://moderation-clusterip-srv:4003/events', event);
 
   res.send({ status: 'OK' });
 });
